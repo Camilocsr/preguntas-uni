709d97d804ffda76c91fe619878a6b4a50b4a433
Camilocsr <esmunred@gmail.com>
2023-01-12 22:00:31 -0500

    actualizacion

--- src/preguntas.js
+++ src/preguntas.js
@@ -1,18 +1,18 @@
 const preguntas = [
     {
-        titulo : "¿?",
+        titulo : "¿Que es una unidad de disco duro?",
         opciones: [
-            {textoRespuesta: "Javascript", isCorret: true},
-            {textoRespuesta: "perro", isCorret: false},
             {textoRespuesta: "Javascript", isCorret: false},
+            {textoRespuesta: "perro", isCorret: false},
+            {textoRespuesta: "Es el medio de almacenamiento más importante del ordenador, permite almacenar y recuperar informacion Sirve para leer y escribir información.", isCorret: true},
             {textoRespuesta: "Javascript", isCorret: false}
         ]
     },
     {
-        titulo : "¿?",
+        titulo : "¿Que es un disco duro SSD?",
         opciones: [
-            {textoRespuesta: "Javascript", isCorret: true},
             {textoRespuesta: "Javascript", isCorret: false},
+            {textoRespuesta: "Son discos duros como si fueran memoria ram pero no son volátiles Son discos duros como si fueran memoria ram pero son volátiles.", isCorret: true},
             {textoRespuesta: "Javascript", isCorret: false},
             {textoRespuesta: "Javascript", isCorret: false}
         ]
